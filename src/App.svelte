0324a31c170c00c383cc1617a79726c226af0930
Jun <118579021+jun42@users.noreply.github.com>
2023-08-10 17:05:31 +0900

    임시커밋

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div>안녕하세요</div>
+</main>
+
+<style>
+div {
+  color:antiquewhite;
+  background-color: black;
+}
+</style>
